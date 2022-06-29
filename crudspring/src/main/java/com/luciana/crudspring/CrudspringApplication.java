e38f8d48a22dbf520619890561bc00f62e8f9775
luluthedevil <luciana.miranda@academico.ifpb.edu.br>
2022-06-29 17:18:12 -0300

    initial commit with starter files from spring

---
+++ crudspring/src/main/java/com/luciana/crudspring/CrudspringApplication.java
@@ -0,0 +1,13 @@
+package com.luciana.crudspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudspringApplication.class, args);
+	}
+
+}
---
+++ crudspring/src/test/java/com/luciana/crudspring/CrudspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciana.crudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
