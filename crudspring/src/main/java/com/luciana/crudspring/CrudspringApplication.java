93cf811794d1209431f01dc5a35356908cb94975
luluthedevil <luciana.miranda@academico.ifpb.edu.br>
2022-06-30 16:58:16 -0300

    Creating data in h2

--- crudspring/src/main/java/com/luciana/crudspring/CrudspringApplication.java
+++ crudspring/src/main/java/com/luciana/crudspring/CrudspringApplication.java
@@ -1,13 +1,40 @@
 package com.luciana.crudspring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.luciana.crudspring.domain.Book;
+import com.luciana.crudspring.domain.Category;
+import com.luciana.crudspring.repositories.BookRepository;
+import com.luciana.crudspring.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class CrudspringApplication {
+public class CrudspringApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@Autowired
+	private BookRepository bookRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudspringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Tech", "IT book");
+
+		Book book1 = new Book(null, "Clean code", "Robert", "Lorem ipsum", cat1);
+
+		cat1.getBooks().addAll(Arrays.asList(book1));
+
+		this.categoryRepository.saveAll(Arrays.asList(cat1));
+		this.bookRepository.saveAll(Arrays.asList(book1));
 	}
 
 }
