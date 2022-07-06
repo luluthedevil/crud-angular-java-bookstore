d6e2b830db709c4b6c2d23eb7172f994924021a3
luluthedevil <luciana.miranda@academico.ifpb.edu.br>
2022-07-06 13:55:11 -0300

    Nav component

--- bookstore-front/src/app/app.module.ts
+++ bookstore-front/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 import { AppMaterialModule } from './shared/app-material/app-material.module';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
--- bookstore-front/src/app/shared/app-material/app-material.module.ts
+++ bookstore-front/src/app/shared/app-material/app-material.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
   ],
 })
 export class AppMaterialModule { }
