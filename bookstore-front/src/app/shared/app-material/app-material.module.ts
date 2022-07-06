0b825054db1f47e97ac165a3e513a81659d07764
luluthedevil <luciana.miranda@academico.ifpb.edu.br>
2022-07-05 21:21:28 -0300

    Created header and shared app-material

---
+++ bookstore-front/src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@NgModule({
+  exports: [
+    MatToolbarModule
+  ],
+})
+export class AppMaterialModule { }
